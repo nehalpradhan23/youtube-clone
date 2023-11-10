9043c53d13cfee56a163d0f8d452bee56ad9f1a7
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-11-10 21:33:55 +0530

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="">
+      <p className=''>hello</p>
+    </div>
+  )
+}
+
+export default App
